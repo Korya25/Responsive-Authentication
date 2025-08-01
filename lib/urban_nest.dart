2d4858e1126aaab4b7c408bd4bcedb241d23821c
Korya25 <????? ??? GitHub>
2025-08-01 21:34:22 +0300

    change app name and split my app in custom file

---
+++ lib/urban_nest.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UrbanNest extends StatelessWidget {
+  const UrbanNest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:responsive_authentication/main.dart';
+import 'package:responsive_authentication/urban_nest.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const UrbanNest());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
